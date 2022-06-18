85beec5e74da1cf49966d80fb4241fe090b8c70a
Frederico Hanai <hanaiitiro@gmail.com>
2022-06-17 22:02:38 -0300

    arrumando3

--- jokenpo.py
+++ jokenpo.py
@@ -3,15 +3,21 @@ from random import randint
 
 
 def start():
-    print(" " * 40)
-    print("J    O    K    E    N    P    Ô")
-    print(' ' * 40)
-    game_mode = int(input("Digite 1 para JOGADOR VS COMPUTADOR\n"
-                          "Digite 2 para JOGADOR VS JOGADOR\n"
-                          "Escolha o modo de jogo:"))
-    if game_mode == 1:
-        return machine()
-    return player_vs_player()
+    while True:
+        print(" " * 40)
+        print("J    O    K    E    N    P    Ô")
+        print(' ' * 40)
+        game_mode = int(input("Digite 1 para JOGADOR VS COMPUTADOR\n"
+                              "Digite 2 para JOGADOR VS JOGADOR\n"
+                              "Escolha o modo de jogo: "))
+        if game_mode == 0:
+            break
+        if game_mode > 2 or game_mode < 0:
+            print('\33[31m Invalido, coloque o numero certo \33[m')
+        elif game_mode == 1:
+            machine()
+        else:
+            player_vs_player()
 
 
 def machine():
@@ -19,51 +25,56 @@ def machine():
     while True:
         game_list = ['Papel', 'Pedra', 'Tesoura']
         print('*' * 40)
-        jogador = int(input("Escolha 0 -> Papel, 1 -> Pedra e 2 -> Tesoura: "))
+        jogador = int(input("Escolha:\n 0 para Papel\n 1 para Pedra\n 2 para Tesoura: "))
         computador = randint(0, 2)
         if jogador > 2 or jogador < 0:
-            print("Seleção incorreta")
+            print("\33[31m Seleção incorreta \33[m")
         elif jogador == computador:
             print(f"Você jogou: {game_list[jogador]} X Computador jogou: {game_list[computador]}")
-            print("JOGADA EMPATADA")
+            print("\33[32m JOGADA EMPATADA \33[m")
         else:
             print(f"Você jogou: {game_list[jogador]} X Computador jogou: {game_list[computador]}")
             print('-' * 40)
             if computador == 2 and jogador == 0:
-                print("Computador ganhou a rodada!")
+                print("\33[33m Computador ganhou a rodada \33[m!")
                 print(' ' * 40)
                 computador_pontos += 1
             else:
                 if computador == 0 and jogador == 1:
-                    print("Computador ganhou a rodada!")
+                    print("\33[33m Computador ganhou a rodada! \33[m")
                     print(' ' * 40)
                     computador_pontos += 1
                 else:
                     if computador == 1 and jogador == 2:
-                        print("Computador ganhou a rodada!")
+                        print("\33[33m Computador ganhou a rodada! \33[m")
                         print(' ' * 40)
                     else:
-                        print("Você ganhou a rodada!!")
+                        print("\33[32m Você ganhou a rodada!! \33[m")
                         print(' ' * 40)
                         jogador_pontos += 1
             if jogador_pontos == 2:
-                print("****VOCÊ GANHOU O JOKENPÔ****")
+                print("\33[32m ****VOCÊ GANHOU O JOKENPÔ**** \33[m")
                 computador_pontos += 1
                 break
             if computador_pontos == 2:
-                print("****COMPUTADOR GANHOU O JOKENPÔ****")
+                print("\33[33m ****COMPUTADOR GANHOU O JOKENPÔ**** \33[m")
                 break
 
 
 def player_vs_player():
     jogador1_pontos = jogador2_pontos = 0
     while True:
-        game_list = ['Papel', 'Pedra', 'Tesoura']
         print('*' * 40)
-        jogador1 = int(input("Digite 0 -> Papel, 1 -> Pedra e 2 -> Tesoura: "))
-        jogador2 = int(input("Digite 0 -> Papel, 1 -> Pedra e 2 -> Tesoura: "))
+        game_list = ['Papel', 'Pedra', 'Tesoura']
+
+        jogador1 = int(input("Jogador 1:\n 0 para Papel\n 1 para Pedra\n 2 para Tesoura: "))
         if jogador1 > 2 or jogador1 < 0:
-            print("Seleção incorreta")
+            print('\33[31m Jogada inválida! Tente novamente.\33[m')
+
+        jogador2 = int(input("Jogador 2:\n 0 para Papel\n 1 para Pedra\n 2 para Tesoura:  "))
+        if jogador2 > 2 or jogador2 < 0:
+            print('\33[31m Jogada inválida! Tenta novamente.\33[m')
+
         elif jogador1 == jogador2:
             print(f"Jogador 1 jogou: {game_list[jogador1]} X Jogador 2 jogou: {game_list[jogador2]}")
             print("JOGADA EMPATADA")
@@ -94,6 +105,8 @@ def player_vs_player():
             if jogador1_pontos == 2:
                 print("****JOGADOR 2 GANHOU O JOKENPÔ****")
                 break
+
+
 
 
 if __name__ == "__main__":
